ad10cdbdce9b4c9629f367dafe337257b7838233
Julian Elmasry <71401589+julian31186@users.noreply.github.com>
2022-08-04 12:36:25 -0400

    Time: 702 ms (5.05%), Space: 48.8 MB (6.92%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ArrayList<ListNode> list = new ArrayList<>();
+        Collections.addAll(list,lists);
+        ListNode head = new ListNode();
+        while(list.remove(null)) {}
+        ListNode temp = head;
+        
+        if(list.isEmpty()) return null;
+               
+        while(!list.isEmpty()) {
+            int min = Integer.MAX_VALUE;
+            ListNode minNode = new ListNode();
+            for(ListNode trav : list) {         
+                if(trav.val < min) {
+                    minNode = trav;
+                    min = trav.val;
+                }
+               
+            }
+            
+            if(minNode.next == null) {
+                list.remove(minNode);
+            } else {
+                list.set(list.indexOf(minNode) , minNode.next);
+            }
+            temp.val = min;
+            if(!list.isEmpty()) temp.next = new ListNode();
+            temp = temp.next;  
+        }
+        
+        return head;
+    }
+}
