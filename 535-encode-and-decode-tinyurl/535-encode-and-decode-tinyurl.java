9d2e2f6dac610d1180635f799c33347dc37163a3
Julian Elmasry <71401589+julian31186@users.noreply.github.com>
2022-08-26 18:12:04 -0400

    Time: 5 ms (63.24%), Space: 42.7 MB (88.49%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
@@ -1,15 +1,21 @@
 public class Codec {
     String shortUrl = "";
+    HashMap<String,String> map = new HashMap<>();
+    
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        return longUrl;
+        int hashCode = longUrl.hashCode();
+        shortUrl = "http://tinyurl.com/" + String.valueOf(hashCode);
+        System.out.println(shortUrl);
+        map.put(shortUrl,longUrl);
+        return shortUrl;
         
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return shortUrl;
         
+        return map.get(shortUrl);
     }
 }
 
