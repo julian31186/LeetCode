e1ed6fe3f9947c29cc87e7a7e50e831157fc8c4c
Julian Elmasry <71401589+julian31186@users.noreply.github.com>
2022-09-08 10:43:00 -0400

    Time: 2 ms (37.33%), Space: 42.7 MB (54.97%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -15,31 +15,32 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-       boolean flag = false;
-       Queue<TreeNode> queue = new LinkedList<>();
-       ArrayList<List<Integer>> lists = new ArrayList<>();
-       queue.add(root);
+        Queue<TreeNode> queue = new LinkedList<>();
+        boolean flag = false;
+        queue.add(root);
+        List<List<Integer>> lists = new ArrayList<>();
         
         if(root == null) return lists;
         
-       while(!queue.isEmpty()) {
-           int size = queue.size();
-           ArrayList<Integer> list = new ArrayList<>();
-           
-           for(int i = 0; i < size; i++) {
-               TreeNode remove = queue.remove();
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            
+            for(int i = 0; i < size; i++) {
+                TreeNode remove = queue.remove();
+                if(!flag) {
+                    list.add(remove.val);
+                } else {
+                    list.add(0, remove.val); 
+                }
+
                 if(remove.left != null) queue.add(remove.left);
-                if(remove.right != null) queue.add(remove.right);
-               
-               if(flag) {
-                   list.add(0, remove.val);
-               } else {
-                   list.add(remove.val);
-               }   
-           }
-           lists.add(list);
-           flag = !flag;
-       }
+                if(remove.right != null)queue.add(remove.right);
+
+            }
+            lists.add(list);
+            flag = !flag;
+        }
         return lists;
     }
 }
