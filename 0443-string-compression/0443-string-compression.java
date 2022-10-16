3e929eec5a9969b0221dce5e66852dbf4466dfc8
Julian Elmasry <71401589+julian31186@users.noreply.github.com>
2022-10-16 16:12:07 -0400

    Time: 22 ms (6.78%), Space: 44.9 MB (12.27%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,63 @@
+class Solution {
+    
+    class Pair {
+        int amount;
+        char c;
+        
+        public Pair(int amount, char c) {
+            this.amount = amount;
+            this.c = c;
+        }
+        
+    }
+    
+    public int compress(char[] chars) {
+        ArrayList<Pair> list = new ArrayList<>();
+        
+        int i = 0;
+        int j = 0;
+        
+        while(i < chars.length) {
+            Pair p = new Pair(0,chars[i]);
+            while(j < chars.length) {
+                if(chars[j] != chars[i]) break;
+                
+                p.amount += 1;
+                j++;
+            }
+            
+            list.add(p);
+            i = j;
+        }
+        
+        int check = 0;
+        for(Pair p : list) {
+            if(p.amount == 1) check +=1;
+            else check +=2;
+        }
+        
+        int L = 0;
+        for(int k = 0; k < list.size(); k++) {
+            Pair p = list.get(k);
+            chars[L++] = p.c;
+            
+            System.out.println(p.amount);
+            
+            if(p.amount == 1)  {
+                continue;
+            } else {
+                String temp = String.valueOf(p.amount);
+                char [] charArr = temp.toCharArray();
+                for(char x : charArr) chars[L++] = x;
+                
+            }
+            
+        }
+
+        
+        return L;
+    }
+}
+
+
+
